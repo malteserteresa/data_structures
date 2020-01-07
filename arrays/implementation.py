36e1dcce53d7f2e75c166b7e4cb9bbb3f0ca7f0e
malteserteresa <tess_the_titan@hotmail.com>
2020-01-07 14:43:27 +0100

    Add core dynamic array functionality

---
+++ arrays/implementation.py
@@ -0,0 +1,45 @@
+import ctypes
+# TODO  insert(index, item), delete(index), remove(item), find(item)
+
+class DynamicArray:
+
+    def __init__(self):
+        self.length_of_array = 0
+        self.initial_capacity = 3
+        self.elements = self.make_array(self.initial_capacity)
+
+    def make_array(self, capacity):
+        return (capacity * ctypes.py_object)()
+
+    def size(self):
+        return self.length_of_array
+
+    def is_empty(self):
+        return True if self.length_of_array == 0 else False
+
+    def append(self, value):
+        if len(self.elements)  == self.length_of_array:
+            self._resize()
+
+        self.elements[self.length_of_array] = value
+        self.length_of_array += 1
+
+    def _resize(self):
+        more_elements = self.make_array(self.length_of_array * 2)
+
+        for i in range(self.length_of_array):
+            more_elements[i] = self.elements[i]
+
+        self.elements = more_elements
+
+    def __getitem__(self, i):
+        if not 0 <= i < self.length_of_array:
+            raise IndexError('i is out of bounds')
+        return self.elements[i]
+
+    def pop(self):
+        if self.length_of_array == 0:
+            raise IndexError('cannot pop empty array')
+        return self.elements[self.length_of_array-1]
+
+
---
+++ arrays/test_implementation.py
@@ -0,0 +1,44 @@
+import pytest
+
+from arrays.implementation import DynamicArray
+
+empty = DynamicArray()
+
+multiple_elements = DynamicArray()
+multiple_elements.append(1.0)
+multiple_elements.append("here")
+multiple_elements.append("there")
+multiple_elements.append("where?")
+
+
+def test_capacity():
+    assert empty.initial_capacity == 3
+    assert multiple_elements.initial_capacity == 3
+
+def test_size():
+    assert empty.size() == 0
+    assert multiple_elements.size() == 4
+
+def test_is_empty():
+    assert empty.is_empty() == True
+    assert multiple_elements.is_empty() == False
+
+def test_add():
+    assert multiple_elements.size() == 4
+
+def test_getitems():
+    assert multiple_elements[0] == 1.0
+    assert multiple_elements[1] == "here"
+    assert multiple_elements[2] == "there"
+    assert multiple_elements[3] == "where?"
+
+    with pytest.raises(IndexError) as ie:
+        multiple_elements[5]
+    assert ie.value.args[0] == "i is out of bounds"
+
+def test_pop():
+    with pytest.raises(IndexError) as ie:
+        empty.pop()
+    assert ie.value.args[0] == "cannot pop empty array"
+
+    assert multiple_elements.pop() == "where?"
