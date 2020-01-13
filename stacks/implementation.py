407235e0e52f9314468bf352336367f64c137e25
malteserteresa <tess_the_titan@hotmail.com>
2020-01-13 11:20:53 +0100

    Implement set of stacks data structure using arrays

--- stacks/implementation.py
+++ stacks/implementation.py
@@ -14,3 +14,29 @@ class Stack:
     def size(self):
         return len(self.stack)
 
+class SetOfStacks:
+
+    def __init__(self):
+        self.capacity = 3
+        self.idx_of_stacks = 0
+        self.stacks = [Stack()]
+
+    def size(self):
+        return self.stacks[self.idx_of_stacks].size() if self.idx_of_stacks == 0 else \
+            (len(self.stacks) - 1) * 3 + self.stacks[-1].size()
+
+    def push(self, item):
+        if self.stacks[self.idx_of_stacks].size() == self.capacity:
+            self.idx_of_stacks += 1
+            self.stacks.append(Stack())
+            self.stacks[self.idx_of_stacks].push(item)
+
+        else:
+            self.stacks[self.idx_of_stacks].push(item)
+
+    def pop(self):
+        if self.stacks[self.idx_of_stacks].size() == 0:
+            self.idx_of_stacks -= 1
+
+        return self.stacks[self.idx_of_stacks].pop()
+
--- stacks/test_implementation.py
+++ stacks/test_implementation.py
@@ -1,4 +1,4 @@
-from stacks.implementation import Stack
+from stacks.implementation import (SetOfStacks, Stack)
 
 empty_stack = Stack()
 filled_stack = Stack()
@@ -13,3 +13,31 @@ def test_size():
 def test_pop():
     assert filled_stack.pop() == "there"
     assert filled_stack.pop() == "here"
+
+
+empty_stacks = SetOfStacks()
+filled_stacks = SetOfStacks()
+filled_stacks.push(1.0)
+filled_stacks.push("here")
+filled_stacks.push("there")
+filled_stacks.push("where?")
+
+
+def test_stacks_capacity():
+    assert empty_stacks.capacity == 3
+
+def test_size():
+    assert empty_stacks.size() == 0
+    assert filled_stacks.size() == 4
+
+def test_index_of_stacks():
+    assert empty_stacks.idx_of_stacks == 0
+    assert filled_stacks.idx_of_stacks == 1
+
+def test_pop_first_stack():
+    assert filled_stacks.pop() == "where?"
+    assert filled_stacks.idx_of_stacks == 1
+
+def test_pop_second_stack():
+    assert filled_stacks.pop() == "there"
+    assert filled_stacks.idx_of_stacks == 0
